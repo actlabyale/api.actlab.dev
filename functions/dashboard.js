5380aa96080c333b8b6501d2bb84d3f791d4be8f
Alex Forrence <alex.forrence@gmail.com>
2023-07-31 14:17:51 -0400

    try parsing xml

--- functions/dashboard.js
+++ functions/dashboard.js
@@ -1,4 +1,4 @@
-
+const parser = require('fast-xml-parser');
 
 export async function onRequest(context) {
     console.log(JSON.stringify(context))
@@ -8,15 +8,18 @@ export async function onRequest(context) {
         return new Response('Missing ticket.', { status: 500 }) // TODO: more appropriate status
     }
     console.log(ticket)
-    const result = await validateCAS(ticket)
+    const { valid, netid } = await validateCAS(ticket)
     return new Response(resp, { status: 200 })
 }
 
 
 async function validateCAS(ticket) {
     const resp = await fetch(`https://secure.its.yale.edu/cas/serviceValidate?ticket=${ticket}&service=https%3A%2F%2Fapi.actlab.dev%2Fdashboard`)
-        .then(response => response.text())
-        .then(str => new DOMParser().parseFromString(str, 'text/xml'))
-        .then(data => console.log(data))
-    return resp
+    const txt = resp.text()
+    if (!parser.validate(txt)) {
+        return { valid: false, netid: '' }
+    }
+    const str = parser.parse(txt)
+    console.log(JSON.stringify(str))
+    return { valid: true, netid: 'foo' }
 }
