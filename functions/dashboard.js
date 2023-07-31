d9a8f4b6bdf286904093ad616e1756c31100c293
Alex Forrence <alex.forrence@gmail.com>
2023-07-31 12:28:05 -0400

    I wish local testing worked...

--- functions/dashboard.js
+++ functions/dashboard.js
@@ -1,3 +1,20 @@
-export function onRequest(context) {
+
+
+export async function onRequest(context) {
+    const url = new URL(request.headers.get('Referer'))
+    const ticket = url.searchParams.get('ticket')
+    if (!ticket) {
+        return new Response('Missing ticket.', { status: 500 }) // TODO: more appropriate status
+    }
+    const result = await validateCAS(ticket)
     return new Response('Fooo')
+}
+
+
+async function validateCAS(ticket) {
+    const resp = await fetch(`https://secure.its.yale.edu/cas/serviceValidate?ticket=${ticket}&service=https%3A%2F%2Fapi.actlab.dev%2Fdashboard`)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
+        .then(data => console.log(data))
+    return resp
 }
